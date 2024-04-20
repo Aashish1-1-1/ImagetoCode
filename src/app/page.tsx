93847501ec9782b874fbb75995c79ba975a7f313
Aashish69-96 <aashishadhikari693@gmail.com>
2024-04-20 11:41:55 +0545

    Typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page(){
  return(
 <form action="../pages/api/form" method="post" >
-      <label htmlFor="first">First Name</label>
+      <label htmlFor="first">Code img</label>
       <input type="file" id="code" name="code" required className="text-black"/>
       <button type="submit">Submit</button>
     </form>
