c8d39efa8c73948681a6933f14c23fa7f00b6018
sannux6 <paharisanjay10@gmail.com>
2024-04-21 16:06:22 -0400

    file to api not working, need to debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,30 @@
 import React, { useState } from 'react';
 export default function Pages(){
 	const [file, setFile] = React.useState();
+	const src = "";
+
 	const handleFileChange = (e) => {
 		    if (e.target.files) {
 		      setFile(e.target.files[0]);
+			  const src = URL.createObjectURL(e.target.files[0])
+			  console.log(src)
+			  console.log(file)
+			  
 		    }
 		};
+
 	const handlesubmit= async (e)=>{
 		e.preventDefault();
 		try{
-			const imgdata = new FormData();
-			imgdata.append('codeimg',file);
+			const formData = new FormData(e.currentTarget);
+			formData.append('codeimg',src);
+			console.log(src)
 			const response = await fetch('/api/backend',{
 			 headers: {
-                            "Content-Type": "multipart/form-data",
+                            "Content-Type": "Content-Type: multipart/form-data; boundary=---WebKitFormBoundary7MA4YWxkTrZu0gW",
                         },
-
 			 method:'POST',
-			 body: imgdata,
+			 body: formData,
 			});
 		if (response.ok) {
     			    const result = await response.json();
@@ -31,7 +38,7 @@ export default function Pages(){
 		}
 	};
    return (
-      <form className="w-full max-w-sm ml-80" onSubmit={handlesubmit}>
+      <form action="" method="post" className="w-full max-w-sm ml-80" onSubmit={handlesubmit}>
          <div className="flex items-center border-b border-teal-500 py-2">
             <input name="codeimg" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="file" placeholder="Add code img" aria-label="codeimg" onChange={handleFileChange} accept="image/*"/>
             <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">Submit</button>
--- src/pages/api/backend.ts
+++ src/pages/api/backend.ts
@@ -1,13 +1,48 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import path from "path";
+import {writeFile} from "fs/promises"
+import { NextResponse } from 'next/server';
+import fs from "fs";
  
 type ResponseData = {
   message: string
 }
- 
-export default function handler(
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+  runtime: 'edge',
+};
+
+
+export default async function POST(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-	console.log(req.body.file)
-  res.status(200).json({ message: 'Image received' })
+  const formData = req.formData();
+  const file = formData.get("file");
+
+  if(!file) {
+    return NextResponse.json({error: "No files recived."}, {status:400});
+  }
+
+  const filename = file.name;
+  
+
+// server ko image lai folder ma save garne
+
+  // try
+  // {
+  //   await writeFile(
+  //     path.join(process.cwd(),"/src/pages" + filename));
+  //     return NextResponse.json({Message: "Success", status: 201});
+  // }
+  // catch (error){
+  //   console.log("Error occured", error);
+  //   return NextResponse.json({Message:"failed", status:500})
+
+  // }
+
+
 }
