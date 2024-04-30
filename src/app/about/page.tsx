8fe2daad905d0c13f05487ca19ff2a2b3dfe19ab
sannux6 <paharisanjay10@gmail.com>
2024-04-30 06:25:17 -0400

    hydration error fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,11 +3,11 @@ import Footer from '../navbar/footer'
 export default function pages(){
 	return(
 	<>
-		<div className="mt-20 ml-16 mr-16">
-			<br/>
+		<div className="mt-28 ml-16 mr-16">
+
 			<h1>We come across many code snippets which are not in actual code format they can be in image or some visual form and what codify does is it convert that visual form of code into actual code and optimizes it if it can be optimized</h1>
-			<br/>
-			<div>Follow us on twitter<br/>
+			
+			<div className='mt-10'>Follow us on twitter<br/>
 			<div className="text-blue-500 flex items-center space-x-4">
 			<a href="https://twitter.com/AashishAdh9">@AashishAdh9</a>
 			<a href="https://twitter.com/AashishAdh9">@Sannux</a></div>
